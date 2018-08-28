3c6b2e56f4b7326a0fc5672e0c226b3013001ca6
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-08-28 12:59:20 +0200

    Have the DB setup in the PL settings.py file, so that I can tweak it easily with `sed` when Dockerizing the app.

--- indigo_pl/settings.py
+++ indigo_pl/settings.py
@@ -1,3 +1,13 @@
 from indigo.settings import *
 
 INSTALLED_APPS = ('indigo_pl',) + INSTALLED_APPS
+
+# This section is copied over from the main settings file, so that when Dockerizing the PL app
+# I can use 'sed' to replace 'postgres://indigo:indigo@localhost:5432/indigo' with the right
+# settings.
+import dj_database_url
+db_config = dj_database_url.config(default='postgres://indigo:indigo@localhost:5432/indigo')
+db_config['ATOMIC_REQUESTS'] = False
+DATABASES = {
+    'default': db_config,
+}
