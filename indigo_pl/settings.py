be9b592cde7442de11a344771842f9a5bfb87097
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-08-30 11:16:24 +0200

    Add USE_TZ=False in Polish settings, because otherwise we get https://stackoverflow.com/questions/38807296

--- indigo_pl/settings.py
+++ indigo_pl/settings.py
@@ -11,3 +11,9 @@ db_config['ATOMIC_REQUESTS'] = False
 DATABASES = {
     'default': db_config,
 }
+
+# Having it set to true resulted in 'database connection isn't set to UTC' AssertionError in Django.
+# See https://stackoverflow.com/questions/38807296 for more. I couldn't find a way to update 
+# the pg_timezone_names view in Postgres responsible for it. We're not really using timestamps
+# in Indigo so this 'hack' seems fine.
+USE_TZ = False
